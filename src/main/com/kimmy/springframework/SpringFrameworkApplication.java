38acccc3b0d4894670b961cd4f97c197e4ac3d50
kimmy <misezhiyan@163.com>
2021-05-17 18:04:07 +0800

    学习同步

--- src/main/com/kimmy/springframework/SpringFrameworkApplication.java
+++ src/main/com/kimmy/springframework/SpringFrameworkApplication.java
@@ -8,19 +8,38 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
+import java.lang.reflect.TypeVariable;
+
 public class SpringFrameworkApplication {
 
     public static void main(String[] args) {
 //        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("F:\\workspaces\\workspace_spring\\springFramework\\src\\resources\\application.xml"));
-        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("F:\\workspaces\\workspace_spring\\springFramework\\src\\resources\\application.xml");
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("F:\\workspace\\workspace_spring\\springFramework\\src\\resources\\application.xml");
 //        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources\\application.xml");
 
+        ApplicationContext applicationContext2 = new FileSystemXmlApplicationContext("F:\\workspace\\workspace_spring\\springFramework\\src\\resources\\application.xml");
 
         TestDao testDao = (TestDao) applicationContext.getBean("testDao");
         TestDao testDao2 = (TestDao) applicationContext.getBean("testDao");
+        TestDao testDaoT = (TestDao) applicationContext2.getBean("testDao");
+        TestDao testDaoT2 = (TestDao) applicationContext2.getBean("testDao");
         System.out.println(testDao);
         System.out.println(testDao2);
+        System.out.println(testDaoT);
+        System.out.println(testDaoT2);
         testDao.add();
+
+        System.out.println("-----------------");
+        System.out.println(applicationContext.getId());
+        System.out.println(applicationContext.getApplicationName());
+        System.out.println(applicationContext.getDisplayName());
+
+        System.out.println("-----------------");
+        TypeVariable<Class<TestTypeVariable>>[] typeParameters = TestTypeVariable.class.getTypeParameters();
+        System.out.println(typeParameters.length);
+    }
+
+    class TestTypeVariable<K,V,T,S>{
 
     }
 }
